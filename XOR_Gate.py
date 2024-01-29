782ed13a8d211a3ebcc3143413728caf165d060a
Bill_Sk <skarlatosbill@gmail.com>
2024-01-29 20:48:24 +0200

    XOR Gate creation and code commenting.

---
+++ XOR_Gate.py
@@ -0,0 +1,4 @@
+def XOR(bit_1, bit_2):
+    if (bit_1==bit_2==0 or bit_1==bit_2==1):
+        return 0
+    return 1
--- key_generator.py
+++ key_generator.py
@@ -1,22 +1,27 @@
-# This will generate 5-bit ranodm keys to only be used once
+# This will generate 5-bit random keys to only be used once.
 import random
+def generateKeys():
+    Num=int(input("How many new keys do you want to generate? (type a negative Number to erase all the previous keys) "))
+    while (Num<0):
+        # Informing the User about how many keys will be deleted. Since every key takes up one line of the .txt file, 
+        # we read the file and count the lines, the result of the count is the number of keys in the file.
+        f=open("secret_book.txt", "r")
+        x = len(f.readlines())
+        print("Deleting ",x," keys ...")
+        f.close()
+        # We open and close the file using the "write" method, which overwrites the keys with nothing.  
+        open("secret_book.txt", "w").close()
+        # These lines are both for generating new keys after deletion and for error prevention.
+        print("--- Please, now select a number over 0 ---")
+        Num=int(input("How many new keys do you want to generate? "))
 
-Num=int(input("How many new keys do you want to generate? (type a negative Number to erase all the previous keys) "))
-while (Num<0):
-    f=open("secret_book.txt", "r")
-    x = len(f.readlines())
-    print("Deleting ",x," keys ...")
+    f = open("secret_book.txt", "a")
+    for i in range(0,Num):
+        key=[]
+        for j in range(0,5):
+            bit_value=random.randrange(0,2)
+            key.append(bit_value)
+        print("New key: ",str(key))
+        f.write(str(key))
+        f.write("\n")
     f.close()
-    open("secret_book.txt", "w").close()
-    print("--- Please select a number over 0 ---")
-    Num=int(input("How many new keys do you want to generate? "))
-f = open("secret_book.txt", "a")
-for i in range(0,Num):
-    key=[]
-    for j in range(0,5):
-        bit_value=random.randrange(0,2)
-        key.append(bit_value)
-    print("New key: ",str(key))
-    f.write(str(key))
-    f.write("\n")
-f.close()
