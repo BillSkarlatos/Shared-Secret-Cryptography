b1b2cfb790354b72a640ff1bf6ab33e39e122458
Bill_Sk <skarlatosbill@gmail.com>
2024-01-29 20:03:44 +0200

    improved key generator handling and added to .gitignore

--- key_generator.py
+++ key_generator.py
@@ -1,9 +1,14 @@
 # This will generate 5-bit ranodm keys to only be used once
 import random
 
-Num=int(input("How many new keys do you want to generate? "))
-while (Num<=0):
-    print("--- Please select a number over 0! ---")
+Num=int(input("How many new keys do you want to generate? (type a negative Number to erase all the previous keys) "))
+while (Num<0):
+    f=open("secret_book.txt", "r")
+    x = len(f.readlines())
+    print("Deleting ",x," keys ...")
+    f.close()
+    open("secret_book.txt", "w").close()
+    print("--- Please select a number over 0 ---")
     Num=int(input("How many new keys do you want to generate? "))
 f = open("secret_book.txt", "a")
 for i in range(0,Num):
@@ -13,4 +18,5 @@ for i in range(0,Num):
         key.append(bit_value)
     print("New key: ",str(key))
     f.write(str(key))
+    f.write("\n")
 f.close()
